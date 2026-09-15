c59e7fb5ecc8f0f07552dd26f5f9fc87d14f3f38
mattandorf1 <mattandorf1@gmail.com>
2026-09-15 09:41:23 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import { createClient } from "@/lib/supabase/server";
+import { createClient } from "../../lib/supabase/client";
 import PlannerApp from "@/components/PlannerApp";
 
 export default async function Home() {
